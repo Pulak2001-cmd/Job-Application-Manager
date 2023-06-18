0e5bc33a092ebbb70a21fc8d5d008516dcda5595
Pulak <pulakkumarghosh2001@gmail.com>
2023-06-18 13:13:38 +0530

    Adding all the files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import HomePage from './components/HomePage';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if(token) {
+      setLoggedIn(true);
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {loggedIn ? (
+        <Routes>
+          <Route path="/" element={<HomePage setLoggedIn={setLoggedIn} />}/>
+        </Routes>
+      ): (
+        <Routes>
+          <Route path='/' element={<Login setLoggedIn={setLoggedIn} />} />
+          <Route path='/signup' element={<Signup setLoggedIn={setLoggedIn} />} />
+        </Routes>
+      )}
+    </BrowserRouter>
   );
 }
 
